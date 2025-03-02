6c5b35c2ba137ed70f16f234fcbb8eef81c6db2e
Ariana Catani <mayerlicatani@gmail.com>
2025-03-01 21:11:58 -0500

    Definición de rutas y controladores del cliente

--- src/database/models/Cliente.js
+++ src/database/models/Cliente.js
@@ -1,13 +1,13 @@
 class Cliente {
     constructor({ id, cedula, nombre, apellido, ciudad, email, direccion, telefono, fecha_nacimiento }) {
         this.id = id;
-        this.cedula;  // Aquí se llama a la validación
+        this.cedula = cedula; 
         this.nombre = nombre;
         this.apellido = apellido;
         this.ciudad = ciudad;
         this.email = email;
         this.direccion = direccion;
-        this.telefono;
+        this.telefono = telefono;
         this.fecha_nacimiento = fecha_nacimiento;
     }
 }
